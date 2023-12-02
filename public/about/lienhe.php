cdb4adec6d3c18169707a72b40cfb4c6359368e5
Huy <huynqph32013@fpt.edu.vn>
2023-12-02 17:15:06 +0700

    huy update

--- public/about/lienhe.php
+++ public/about/lienhe.php
@@ -25,7 +25,7 @@
         </div>
         <div class="col-6">
             <!--Google map-->
-            <div id="map-container-google-1" class="z-depth-1-half map-container" style="height: 500px">
+            <div id="map-container-google-1" class="z-depth-1-half map-container" style="height: 500px; width: 700px">
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.8353521256568!2d105.72923707588805!3d21.039272987416215!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31345550b525aa03%3A0x3fdefc40f69a023a!2zQ2FvIMSR4bqzbmcgRlBUIFBo4buRIFRy4buLbmggVsSDbiBCw7QgLCBQaMaw4budbmcgUGjGsMahbmcgQ2FuaCAsIHF14bqtbiBU4burIExpw6pt!5e0!3m2!1svi!2s!4v1701506304565!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
 
--- public/config/addcombo.php
+++ public/config/addcombo.php
@@ -1,80 +1,80 @@
-<?php if(isset($_SESSION['comb'])&& ($_SESSION['comb'])){?>
-    
-<h3>combo</h3>
-<?php 
-                   if(isset($_SESSION['comb'])){
-                    $i = 0;
-                    $ttien = 0;
-                    $sl = 1;
-                    foreach($_SESSION['comb'] as $cart){
-                       
-                    $hinh = '../uploads/'.$cart[3];
-              
-                    ?>
-                    <div style="text-align: center;margin-top:50px;" class="row">
-                        <div class="col-3"><img width="50px" src="<?php echo $hinh?>" alt="hehe"></div>
-                        <div class="col-5"><p style="line-height: 50px;"><?php echo $cart[1]?></p></div>
-                        <div class="col-4"><p style="line-height: 50px;text-decoration:line-through;color:red"><?php echo number_format($cart[5])?> VND</p><?php echo number_format($cart[5]-($cart[5]*$cart[2] / 100))?> VND</p></div>
-                    </div>
-                    <div style="margin-top:10px" class="row">
-                        <div class="col-4"> Số Lượng  </div>
-                        <div class="col-5">
-                            <button class="btnupd" onclick="giam(this)"> - </button><span id="sluongg"><?php echo $sl?></span><button class="btnupd" onclick="tang(this)"> + </button>
-                        </div>
-                        <div class="col-3"><a href="?act=delcombo&idhd=<?php echo $i?>">Xóa</a></div>
-                    </div>
-                  
-                    <?php
-                
-                
-                
-                    $i++;
-                   } 
-                   ?>
-                        <?php if(isset($_SESSION['comb'])&&($_SESSION['comb'])):?>
-                            <div style="margin-top: 50px;" class="row">
-                            <div class="col-7"></div>
-                            <div class="col-5"><button type="button" class="btn btn-primary" data-mdb-ripple-init>Lưu</button></div>
-                        </div>
-                        <?php endif;?>
-                    <hr>
-                    <div class="row">
-                        <div class="col-4"><p>Số Lượng :</p></div>
-                        <div class="col-2"><p><?php echo $sl?></p></div>
-                        <div class="col-6"><p>Tổng Tiền :</p> <?php echo number_format($ttien)?> VND</div>
-                    </div>
-                            
+<?php if (isset($_SESSION['comb']) && ($_SESSION['comb'])) { ?>
 
-                   <?php
-                
-                }
-?>
+    <h3>combo</h3>
+    <?php
+    if (isset($_SESSION['comb'])) {
+        $i = 0;
+        $thanhtien = 0;
+        $sluong = 0;
+        foreach ($_SESSION['comb'] as $cart) {
 
-<script>
-   function tang(x){
-    var cha = x.parentElement;
-    var slcu = cha.children[1];
-    var slm = parseInt(slcu.innerText) + 1;
-    slcu.innerText = slm;
-    updategia();
-   
+            $hinh = '../uploads/' . $cart[3];
 
-   }
+    ?>
+            <div style="text-align: center;margin-top:50px;" class="row">
+                <div class="col-3"><img width="50px" src="<?php echo $hinh ?>" alt="hehe"></div>
+                <div class="col-5">
+                    <p style="line-height: 50px;"><?php echo $cart[1] ?></p>
+                </div>
+                <div class="col-4">
+                    <p style="line-height: 50px;text-decoration:line-through;color:red"><?php echo number_format($cart[5]) ?> VND</p><?php echo number_format($cart[5] - ($cart[5] * $cart[2] / 100)) ?> VND</p>
+                </div>
+            </div>
+            <div style="margin-top:10px" class="row">
+                <div class="col-4"> Số Lượng </div>
+                <div class="col-5">
+                    <button class="btnupd" onclick="giam(this)"> - </button><span id="sluongg"><?php echo $cart[6] ?></span><button class="btnupd" onclick="tang(this)"> + </button>
+                </div>
+                <div class="col-3"><a href="?act=delcombo&idhd=<?php echo $i ?>">Xóa</a></div>
+            </div>
+
+        <?php
 
 
-   function updategia(){
-        var sl = $("#sluongg").text();
-        console.log(sl);
-   }
+
+            $i++;
+        }
+        ?>
+        <?php if (isset($_SESSION['comb']) && ($_SESSION['comb'])) : ?>
+            <div style="margin-top: 50px;" class="row">
+                <div class="col-7"></div>
+                <div class="col-5"><button type="button" class="btn btn-primary" data-mdb-ripple-init>Lưu</button></div>
+            </div>
+        <?php endif; ?>
 
 
-   function giam(x){
-    var cha = x.parentElement;
-    var slcu = cha.children[1];
-    if(parseInt(slcu.innerText)>1){
-    var slm = parseInt(slcu.innerText) - 1;
-    slcu.innerText = slm;}
-    updategia();
-   }
-</script>
-    <?php } ?>
+
+    <?php
+
+    }
+    ?>
+
+    <script>
+        function tang(x) {
+            var cha = x.parentElement;
+            var slcu = cha.children[1];
+            var slm = parseInt(slcu.innerText) + 1;
+            slcu.innerText = slm;
+            updategia();
+
+
+        }
+
+
+        function updategia() {
+            var sl = $("#sluongg").text();
+            console.log(sl);
+        }
+
+
+        function giam(x) {
+            var cha = x.parentElement;
+            var slcu = cha.children[1];
+            if (parseInt(slcu.innerText) > 1) {
+                var slm = parseInt(slcu.innerText) - 1;
+                slcu.innerText = slm;
+            }
+            updategia();
+        }
+    </script>
+<?php } ?>
